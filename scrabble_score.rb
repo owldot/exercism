f19331d1ef41c94a12774f87c19982489d16da5a
lanadz <svitlana.dzyuban@gmail.com>
2020-02-22 16:32:14 -0500

    Add scrabble

---
+++ scrabble_score.rb
@@ -0,0 +1,50 @@
+class Scrabble
+  RULES = {
+    'A' => 1,
+    'E' => 1,
+    'I' => 1,
+    'O' => 1,
+    'U' => 1,
+    'L' => 1,
+    'N' => 1,
+    'R' => 1,
+    'S' => 1,
+    'T' => 1,
+    'D' => 2,
+    'G' => 2,
+    'B' => 3,
+    'C' => 3,
+    'M' => 3,
+    'P' => 3,
+    'F' => 4,
+    'H' => 4,
+    'V' => 4,
+    'W' => 4,
+    'Y' => 4,
+    'K' => 5,
+    'J' => 8,
+    'X' => 8,
+    'Q' => 10,
+    'Z' => 10
+  }
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def score
+    return 0 if word.nil? || word.gsub(/\s/, '').empty?
+
+    word.upcase.chars.reduce(0) do |score, char|
+      score + RULES[char]
+    end
+  end
+
+  def self.score(word)
+    scrabble = self.new(word)
+    scrabble.score
+  end
+
+end
